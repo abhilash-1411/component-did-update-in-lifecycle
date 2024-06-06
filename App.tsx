63dcdbd52194ebe878b086fa385e839c72770124
abhilash-1411 <Abhilash.Chandravanshi@adixoo.com>
2024-06-06 14:46:46 +0530

    componentDidUpdate Using useEffect hook

--- App.tsx
+++ App.tsx
@@ -1,118 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+
+import React, { useState,useEffect } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
+ Button,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+   
+const App=()=> {
+  const [count ,setCount]=useState(0);
+  const [data,setData]=useState(100)
+//   useEffect(()=>{
+//      console.warn("Do some animations")
+//   },[count])
+
+//   useEffect(()=>{
+//     console.warn("call some api here")
+//  },[data])
+
+ 
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text style={{fontSize:25}}> {data} :useEffect as componentDidUpdate:{count}</Text>
+      <Button title='Update Counter' onPress={()=>setCount(count+1)}/>
+      <Button title='Update Data' onPress={()=>setData(data+1)}/>
+      <User info={{count,data}}/>
     </View>
+        
   );
 }
+const User=(props)=>{
+  useEffect(()=>{
+    console.warn("run this code when data prop is updated")
+   
+  },[props.info.data])
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  useEffect(()=>{
+    console.warn("run this code when count prop is updated")
+   
+  },[props.info.count])
+  return(
+  <View>
+  <Text style={{fontSize:30,color:'orange'}}> Data:{props.info.data}</Text>
+  <Text style={{fontSize:30,color:'orange'}}> Count:{props.info.count}</Text>
+   </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
